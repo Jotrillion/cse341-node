7d664417a46dee55f64fd09bb056a340134a0fd9
jotrillion <josaphatngandu1@gmail.com>
2026-03-29 01:38:33 +0100

    added cors required

--- cse341project2/server.js
+++ cse341project2/server.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const routes = require('./routes');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
